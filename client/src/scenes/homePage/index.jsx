4f7fb7d8f5d351b051772b64d8cc2ff507bbc65c
Raj Thombare <therajthombare@gmail.com>
2023-12-28 13:01:18 +0530

    feat: user profile page

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -4,6 +4,8 @@ import { useSelector } from "react-redux";
 import UserWidget from "../widgets/UserWidget";
 import CreatePost from "../widgets/CreatePost";
 import PostsWidget from "../widgets/PostsWidget";
+import AdvertWidget from "../widgets/AdvertWidget";
+import FriendListWidget from "../widgets/FriendListWidget";
 
 const HomePage = () => {
   const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
@@ -29,7 +31,13 @@ const HomePage = () => {
           <CreatePost picturePath={picturePath} />
           <PostsWidget userId={_id} />
         </Box>
-        {isNonMobileScreens && <Box flexBasis="26%"></Box>}
+        {isNonMobileScreens && (
+          <Box flexBasis="26%">
+            <AdvertWidget />
+            <Box m="2rem 0" />
+            <FriendListWidget userId={_id} />
+          </Box>
+        )}
       </Box>
     </Box>
   );
